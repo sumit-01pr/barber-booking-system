ca213ade36618781a4bff7bb8e919242cfaeca85
sumit-01pr <sumitprajapati3935@gmail.com>
2026-05-26 12:57:29 +0530

    Setup frontend authentication architecture

---
+++ client/src/services/authService.js
@@ -0,0 +1,28 @@
+import axiosInstance
+from "../api/axios";
+
+export const registerUser =
+  async (userData) => {
+
+    const response =
+      await axiosInstance.post(
+        "/auth/register",
+        userData
+      );
+
+    return response.data;
+
+  };
+
+export const loginUser =
+  async (userData) => {
+
+    const response =
+      await axiosInstance.post(
+        "/auth/login",
+        userData
+      );
+
+    return response.data;
+
+  };
